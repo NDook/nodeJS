2637b02dc63ed12d95ed952ae7724bad486d2f87
NDook <thomasmludek@gmail.com>
2022-05-09 14:24:45 +0200

    it's ok

---
+++ index.js
@@ -0,0 +1,11 @@
+import chalk from 'chalk'
+const jeanLau = "Jean-Laurent";
+const flo = "Flo";
+const boum = "Boum";
+const damien = "Damien";
+
+console.log(chalk.blue(jeanLau));
+console.log(chalk.red(flo));
+console.log(chalk.green(boum));
+console.log(chalk.yellow(damien));
+
